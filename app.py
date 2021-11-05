a055983dbb0b7a0fc51fb5369f0453f1626c4ec2
florencia <fldiaz@itba.edu.ar>
2021-11-04 21:28:11 -0300

    streamlit

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-# http://ec2-52-13-29-251.us-west-2.compute.amazonaws.com:5000/
-# aidan
+import streamlit as st
+
+import spacy_streamlit
 from urllib import request
 from io import StringIO
 import json
 import sys
 import os
 import re
-
 from nltk.tokenize import sent_tokenize, word_tokenize
-from sentence_transformers import SentenceTransformer
-from flask import Flask, request, render_template
+from sentence_transformers import SentenceTransformer, util
 from nltk.probability import FreqDist
 from nltk.stem import LancasterStemmer
 from nltk.stem import PorterStemmer
@@ -28,20 +27,31 @@ nltk.download('punkt')
 # https://www.sbert.net/docs/pretrained_models.html
 sentence_model = SentenceTransformer("all-mpnet-base-v2")
 
-###florencia 
-app = Flask(__name__)
 
-data = None
-article_df = None
+st.title('GLG project')
+spacy_model = "en_core_web_sm"
+DEFAULT_TEXT='complete text'
+text = st.text_area("Text to analyze", DEFAULT_TEXT, height=200)
+doc = spacy_streamlit.process_text(spacy_model, text)
 
-if data is None:
-    #data = pd.read_csv('https://word-emeddings.s3.us-west-2.amazonaws.com/20211017_embeddings_saved_FULL.csv', index_col=0)
-    data = pd.read_csv('https://word-emeddings.s3.us-west-2.amazonaws.com/20211031_weighted_embeddings_saved_PARTIAL.csv')
+spacy_streamlit.visualize_ner(
+    doc,
+    labels=['ORG','GPE','MONEY','EVENT', 'LAW'],
+    title="Named Entities",
+    show_table=False
+)
 
-if article_df is None:
-    article_df = pd.read_csv('https://word-emeddings.s3.us-west-2.amazonaws.com/20211024_main_article_dataframe.csv')
 
-# The last embedding was not part of the dataset originally, getting rid of it
+@st.cache
+def load_data(file):
+    data = pd.read_csv(file, index_col=0)
+    return data
+
+
+data = load_data('pesos.csv')
+
+
+article_df = load_data('datajstore.csv')
 article_df = article_df.loc[:article_df.shape[0]-2,:]
 
 
@@ -54,7 +64,7 @@ def strip_punctuation_stopwords(token_list):
                 and word not in string.punctuation \
                 and len(word) > 1]
 
-
+st.subheader('Documents')
 def get_weighted_embedding(token_list_long):
     text_token = word_tokenize(token_list_long)
     text_token_clean = strip_punctuation_stopwords(text_token)
@@ -68,81 +78,35 @@ def get_weighted_embedding(token_list_long):
     final = pd.DataFrame(weighted_embed.sum(axis=0))
     return final
 
-
-@app.route('/')
-def my_form():
-    df_html = data.describe().to_html()
-    return render_template('my-form.html',
-                            df_html=df_html,
-                            df_shape='1')
-
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['text']
-    print('HERE IS THE POST')
-    print(text)
-    weighted_embedding = get_weighted_embedding(text)
-    weighted_embedding2 = weighted_embedding.T
-    weighted_embedding2.columns = data.columns
-    print(data.head())
-    print(data.tail())
-    print(weighted_embedding2)
-    print(weighted_embedding2.columns, file=sys.stderr)
-    data2 = pd.concat([data, weighted_embedding2]).reset_index(drop=True)
-    print(data2.head())
-    print(data2.tail())
-    print('DID IT MAKE SENSE?')
-    # CLUSTERING
-    #kmeans = KMeans(n_clusters = 20, random_state = 1111)
-    #clusters = kmeans.fit_predict(data2)
-    #data2['clusters'] = clusters
-    #print(data2.clusters.value_counts(), file=sys.stderr)
-    dist_mat = distance_matrix(data2, data2)
-    dist_mat = pd.DataFrame(dist_mat)
-    print('dist_mat - tail')
-    print(dist_mat.tail(10))
-    print(dist_mat.tail(1).T)
-    data2['dist_2_new'] = dist_mat.tail(1).T
-    print('data2 head BEFORE SORT')
-    print(data2.head())
-    print('data2 tail BEFORE SORT')
-    print(data2.tail())
-    data2 = data2.sort_values('dist_2_new')
-    print('data2 head:')
-    print(data2.head())
-    print('data2 tail after sort:')
-    print(data2.tail())
-    article_index = [x for x in data2.head().index if x != 1200]
-    dataF = article_df.loc[article_index,:].copy()
-    dataF['first'] = dataF.fullText.apply(lambda x: x[0:3000])
-    dataF['last'] = dataF.fullText.apply(lambda x: x[-3000:])
-    dataF = dataF.loc[:,['abstract', 'creator', 'datePublished', 'docType', 'first', 'last', 'id',
-       'identifier', 'isPartOf', 'outputFormat', 'provider', 'publicationYear',
-       'title', 'url', 'wordCount', 'language', 'pageCount', 'pageEnd', 'pageStart',
-       'pagination', 'sourceCategory', 'subTitle', 'tdmCategory', 'collection',
-       'hasPartTitle', 'publisher']]
-    #for index, row in data2.head(10).iterrows():
-    #    print('----------------')
-    #    print(index)
-    #    print(f'Article')
-    #    ting = article_df.iloc[index,:]
-    #    print(ting['fullText'][0:1000])
-    #print(data2.head().index)
-    return render_template('my-form.html',
-                            df_html=dataF.to_html())
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-
-# from flask import Flask
-#
-# app = Flask(__name__)
-#
-# @app.route('/')
-# def index():
-#     return 'Web App with Python Flask!'
-#
-# app.run(host='0.0.0.0')
+weighted_embedding = get_weighted_embedding(text)
+weighted_embedding2 = weighted_embedding.T
+weighted_embedding2.columns = data.columns
+print(data.head())
+print(data.tail())
+print(weighted_embedding2)
+print(weighted_embedding2.columns, file=sys.stderr)
+data2 = pd.concat([data, weighted_embedding2]).reset_index(drop=True)
+print(data2.head())
+print(data2.tail())
+print('DID IT MAKE SENSE?')
+dist_mat = distance_matrix(data2, data2)
+dist_mat = pd.DataFrame(dist_mat)
+print('dist_mat - tail')
+print(dist_mat.tail(10))
+print(dist_mat.tail(1).T)
+data2['dist_2_new'] = dist_mat.tail(1).T
+print('data2 head BEFORE SORT')
+print(data2.head())
+print('data2 tail BEFORE SORT')
+print(data2.tail())
+data2 = data2.sort_values('dist_2_new')
+print('data2 head:')
+print(data2.head())
+print('data2 tail after sort:')
+print(data2.tail())
+article_index = [x for x in data2.head().index if x != 1200]
+dataF = article_df.loc[article_index,:].copy()
+dataF['first'] = dataF.fullText.apply(lambda x: x[0:3000])
+dataF['last'] = dataF.fullText.apply(lambda x: x[-3000:])
+dataF = dataF.loc[:,['abstract', 'creator', 'datePublished']]
+st.dataframe(dataF)
